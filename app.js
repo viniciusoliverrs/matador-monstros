c0302d04a945be4b95dea647ce0f1a0c831a6241
Vinícius Pereira de Oliveira <vipoliveira2012@live.com>
2022-03-26 12:24:11 -0300

    feat: Ajuste na condição se o player ganhou ou perdeu.

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerLife: 100,
+        monsterLife: 0
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife === 0 || this.monsterLife === 0
+        }
+    },
+    methods: {
+        
+    },
+    watch: {
+
+    }
+})
